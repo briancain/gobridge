97b8b3c22ec5a9ba729cd1f56204877fa78e3bea
Brian Cain <brian.cain@puppetlabs.com>
2017-04-18 14:49:18 -0700

    Set up server and push out bridge updates

--- src/bridge.go
+++ src/bridge.go
@@ -3,9 +3,11 @@ package main
 import (
   "encoding/json"
   "io/ioutil"
+  "fmt"
   "log"
   "net/http"
   "reflect"
+  "github.com/dghubble/go-twitter/twitter"
 )
 
 const bridgeAPI string = "https://api.multco.us"
@@ -37,23 +39,44 @@ func readBridgeData(data []byte) map[string]bool {
     bridgeMap[name] = isUp
   }
 
-  toString(bridgeMap)
+  //toString(bridgeMap)
   return bridgeMap
 }
 
-func checkState(oldBridges map[string]bool, newBridges map[string]bool) map[string]bool {
+func checkState(oldBridges map[string]bool, newBridges map[string]bool, twitterClient *twitter.Client) map[string]bool {
   if !reflect.DeepEqual(oldBridges, newBridges) {
     for bridge,status := range newBridges {
       if oldBridges[bridge] != status {
         log.Printf("Bridge %s has changed to %t", bridge, status)
+        var returnString = ""
+        if status == true {
+          returnString = fmt.Sprintf("Bridge %s has been raised", bridge)
+        } else if status == false {
+          returnString = fmt.Sprintf("Bridge %s has been lowered", bridge)
+        } else {
+          log.Println("Could not determine bridge status")
+          return oldBridges
+        }
+
+        log.Println("Sending tweet: ", returnString)
+        tweet, resp, err := twitterClient.Statuses.Update(returnString, nil)
+
+        if err != nil {
+          log.Println("Something went wrong making a twitter request")
+          log.Println(tweet)
+          log.Println(resp)
+          log.Println(err)
+        }
       }
     }
+  } else {
+    log.Println("No change in bridge statuses")
   }
 
   return newBridges
 }
 
-func getStatus(bridges map[string]bool, token string) (map[string]bool, error) {
+func getStatus(bridges map[string]bool, token string, twitterClient *twitter.Client) (map[string]bool, error) {
   var allBridgeURL = bridgeAPI + "/bridges?access_token=" + token
   resp, err := http.Get(allBridgeURL)
 
@@ -70,7 +93,7 @@ func getStatus(bridges map[string]bool, token string) (map[string]bool, error) {
   }
 
   var bridgesReturn = readBridgeData(body)
-  var newState = checkState(bridges, bridgesReturn)
+  var newState = checkState(bridges, bridgesReturn, twitterClient)
 
   return newState, nil
 }
--- src/main.go
+++ src/main.go
@@ -2,10 +2,17 @@ package main
 
 import (
   "log"
+  "fmt"
+  "strings"
+  "net/http"
+  "time"
+
   "github.com/spf13/viper"
+  "github.com/dghubble/go-twitter/twitter"
+  "github.com/dghubble/oauth1"
 )
 
-func readConfig() string{
+func readConfig() *viper.Viper {
   log.Println("Reading config")
   v := viper.New()
   v.SetConfigName("bridge")
@@ -17,15 +24,27 @@ func readConfig() string{
     log.Fatal(err)
   }
 
-  token := v.GetString("apikey")
-  return token
+  return v
 }
 
-func startServer(token string) {
+func makeRequest(token string, initBridge map[string]bool, twitterClient *twitter.Client) map[string]bool {
+  // init data
+
+  var resp, err = getStatus(initBridge, token, twitterClient)
+  if err != nil {
+    log.Fatal(err)
+  } else {
+    bridges := resp
+    log.Println(bridges)
+    return bridges
+  }
+  return nil
+}
+
+func startServer(token string, twitterClient *twitter.Client) {
   log.Println("Starting gobridge server")
   log.Println("Getting bridge status")
 
-  // init data
   bridges := map[string]bool {
     "hawthorne": false,
     "morrison": false,
@@ -34,18 +53,54 @@ func startServer(token string) {
     "I-5": false,
   }
 
-  var resp, err = getStatus(bridges, token)
-  if err != nil {
-    log.Fatal(err)
-  } else {
-    bridges = resp
-    log.Println(bridges)
+  for {
+    time.Sleep(2 * time.Second)
+    bridges = makeRequest(token, bridges, twitterClient)
   }
+}
+
+func sendVersion(w http.ResponseWriter, r *http.Request) {
+  r.ParseForm()  // parse arguments, you have to call this by yourself
+  fmt.Println(r.Form)  // print form information in server side
+  fmt.Println("path", r.URL.Path)
+  fmt.Println("scheme", r.URL.Scheme)
+  fmt.Println(r.Form["url_long"])
+  for k, v := range r.Form {
+      fmt.Println("key:", k)
+      fmt.Println("val:", strings.Join(v, ""))
+  }
+  fmt.Fprintf(w, "Hello gobridge!") // send data to client side
+}
+
+func setupTwitterClient(config *viper.Viper) *twitter.Client {
+  twitterConsumerKey := config.GetString("twitter.consumerkey")
+  twitterConsumerSecret := config.GetString("twitter.consumersecret")
+  twitterAccessToken := config.GetString("twitter.accesstoken")
+  twitterAccessSecret := config.GetString("twitter.accesstokensecret")
+
+  twitterConfig := oauth1.NewConfig(twitterConsumerKey, twitterConsumerSecret)
+  twitterToken := oauth1.NewToken(twitterAccessToken, twitterAccessSecret)
+
+  httpClient := twitterConfig.Client(oauth1.NoContext, twitterToken)
+  client := twitter.NewClient(httpClient)
+
+  return client
 }
 
 func main() {
   log.Println("Starting gobridge server")
-  token := readConfig()
+  viperConfig := readConfig()
 
-  startServer(token)
+  token := viperConfig.GetString("apikey")
+  twitterClient := setupTwitterClient(viperConfig)
+
+  //tweet, resp, terr := twitterClient.Statuses.Update("Test test", nil)
+
+  go startServer(token, twitterClient)
+
+  http.HandleFunc("/", sendVersion)
+  err := http.ListenAndServe(":9090", nil)
+  if err != nil {
+    log.Fatal("ListenAndServe:", err)
+  }
 }
